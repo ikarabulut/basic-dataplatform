c466e6a8d8eda26c7804d8717556d03591a93cba
Ismail Karabulut <i.karabulut93@gmail.com>
2024-05-21 18:51:06 -0500

    replace api with springboot skeleton

---
+++ platform-api/src/test/java/com/ikarabulut/platformapi/PlatformApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ikarabulut.platformapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
